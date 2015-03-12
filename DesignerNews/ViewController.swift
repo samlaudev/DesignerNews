d97cb3c70f01294db5f4153634cbfe5ebc879bb1
Sam Lau <liuyaozhu13hao@163.com>
2015-03-12 12:55:12 +0800

    add git version control support

---
+++ DesignerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignerNews
+//
+//  Created by Sam Lau on 3/12/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
